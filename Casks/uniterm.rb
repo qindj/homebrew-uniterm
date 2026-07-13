00fbf68b67f0dc0ad45db52b4254b7cd117dcc09
zhanglq <zhanglq2@sugon.com>
2026-07-13 14:08:48 +0800

    fix: use single sha256 for universal DMG

--- Casks/uniterm.rb
+++ Casks/uniterm.rb
@@ -1,7 +1,6 @@
 cask "uniterm" do
   version "1.4.1"
-  sha256 arm:   "40f01eb2b8ae1f6f90669de8f41828c581ac8ecb38b94886bec7a4f7c82c35b8"
-          intel: "40f01eb2b8ae1f6f90669de8f41828c581ac8ecb38b94886bec7a4f7c82c35b8"
+  sha256 "40f01eb2b8ae1f6f90669de8f41828c581ac8ecb38b94886bec7a4f7c82c35b8"
 
   url "https://github.com/ys-ll/uniterm/releases/download/v#{version}/uniterm-darwin-universal-v#{version}.dmg"
   name "uniTerm"
