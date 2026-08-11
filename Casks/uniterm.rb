8651ef2ce3ffb70c628b2bdb5734efd3ed3e6d0b
ys-ll <114486081+ys-ll@users.noreply.github.com>
2026-08-11 16:21:02 +0800

    Update sha256 checksums for uniterm cask

--- Casks/uniterm.rb
+++ Casks/uniterm.rb
@@ -2,8 +2,8 @@ cask "uniterm" do
   arch arm: "arm64", intel: "amd64"
 
   version "1.7.0"
-  sha256 arm: "1ea393e61d77e300f6e10fc430d143d08e58c7c457b4fd6eb252644781f016e1",
-         intel: "86f2bb8605167481c1e55a09becfc9ee596a29132971f05faf6d3e46fa848289"
+  sha256 arm: "c56c5f4e69e3d74c5402c323064c9caf444f958848d0088d68b63750b8b4e034",
+         intel: "ecaf24298aaf6252a0e7d732818d327178dd4233f5fd9600a60a2ba11e294d9d"
 
   url "https://github.com/ys-ll/uniterm/releases/download/v#{version}/uniterm-darwin-#{arch}-v#{version}.dmg"
   name "uniTerm"
